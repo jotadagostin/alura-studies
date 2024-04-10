edb9b6f8977d61c3b0ef1f56530cf031c6284d8b
João Paulo Dagostin <dagostinjota@gmail.com>
2024-04-10 12:32:28 +0200

    Update index.tsx
    controlling the form

--- alura-studies/src/components/Form/index.tsx
+++ alura-studies/src/components/Form/index.tsx
@@ -3,15 +3,29 @@ import style from "./Form.module.scss";
 import Button from "../Button";
 
 class Form extends React.Component {
+  state = {
+    task: "",
+    time: "00:00",
+  };
+
+  addTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log("state: ", this.state);
+  }
+
   render() {
     return (
-      <form className={style.newTask}>
+      <form className={style.newTask} onSubmit={this.addTask}>
         <div className={style.inputContainer}>
           <label htmlFor="task">Add a new study</label>
           <input
             type="text"
             name="task"
             id="task"
+            value={this.state.task}
+            onChange={(event) =>
+              this.setState({ ...this.state, task: event.target.value })
+            }
             placeholder="What do you want to study?"
             required
           />
@@ -22,6 +36,10 @@ class Form extends React.Component {
             type="time"
             step="1"
             name="time"
+            value={this.state.time}
+            onChange={(event) =>
+              this.setState({ ...this.state, time: event.target.value })
+            }
             id="time"
             min="00:00:00"
             max="01:30:00"
