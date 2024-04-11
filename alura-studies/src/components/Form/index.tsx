cbb27326a6225aa7a837aa9a35e88ff422954c5b
João Paulo Dagostin <dagostinjota@gmail.com>
2024-04-11 11:38:53 +0200

    undpate index.tsx
    update task

--- alura-studies/src/components/Form/index.tsx
+++ alura-studies/src/components/Form/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import style from "./Form.module.scss";
 import Button from "../Button";
 import { ITask } from "../../types/task";
+import { v4 as uuidv4 } from "uuid";
 
 class Form extends React.Component<{
   setTasks: React.Dispatch<React.SetStateAction<ITask[]>>;
@@ -13,7 +14,10 @@ class Form extends React.Component<{
 
   addTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    this.props.setTasks((oldTasks) => [...oldTasks, { ...this.state }]);
+    this.props.setTasks((oldTasks) => [
+      ...oldTasks,
+      { ...this.state, selected: false, completed: false, id: uuidv4() },
+    ]);
     this.setState({
       task: "",
       time: "00:00",
--- alura-studies/src/components/List/Item/index.tsx
+++ alura-studies/src/components/List/Item/index.tsx
@@ -1,6 +1,9 @@
+import { ITask } from "../../../types/task";
 import style from "../List.module.scss";
 
-export default function Item({ task, time }: { task: string; time: string }) {
+export default function Item({ task, time, selected, completed, id }: ITask) {
+  console.log("Actual item: ", { task, time, selected, completed, id });
+
   return (
     <li className={style.item}>
       <h3>{task}</h3>
