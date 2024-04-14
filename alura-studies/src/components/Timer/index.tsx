7236ef467b0ad6eaeeebc1bee55c5f3f9668eeff
João Paulo Dagostin <dagostinjota@gmail.com>
2024-04-14 16:44:23 +0200

    update jsx
    starting the timer code

--- alura-studies/src/components/Timer/index.tsx
+++ alura-studies/src/components/Timer/index.tsx
@@ -1,14 +1,23 @@
-import { timeForSeconds } from "../../common/utils/date";
+import { useState } from "react";
+import { timeForSeconds } from "../../common/utils/time";
+import { ITask } from "../../types/task";
 import Button from "../Button";
 import Clock from "./Clock";
 import style from "./Timer.module.scss";
 
-export default function Timer() {
-  console.log("converting: ", timeForSeconds("01:01:01"));
+interface Props {
+  selected: ITask | undefined;
+}
 
+export default function Timer({ selected }: Props) {
+  const [time, setTime] = useState<number>();
+  if (selected?.time) {
+    setTime(timeForSeconds(selected.time));
+  }
   return (
     <div className={style.timer}>
       <p className={style.title}>Choose a card and start the timer</p>
+      Time: {time}
       <div className={style.clockWrapper}>
         <Clock />
       </div>
