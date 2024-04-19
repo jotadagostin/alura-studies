77aac0a3bced961913c226402435e51125ad51ac
João Paulo Dagostin <dagostinjota@gmail.com>
2024-04-19 12:05:21 +0200

    update tsx code
    form and button: setting class components

--- alura-studies/src/components/Button/index.tsx
+++ alura-studies/src/components/Button/index.tsx
@@ -1,19 +1,33 @@
 import React from "react";
 import style from "./Button.module.scss";
 
-class Button extends React.Component<{
-  children: string;
+interface Props {
   type?: "button" | "submit" | "reset" | undefined;
   onClick?: () => void;
-}> {
-  render() {
-    const { type = "button", onClick } = this.props;
-    return (
-      <button onClick={onClick} type={type} className={style.button}>
-        {this.props.children}
-      </button>
-    );
-  }
+  children?: React.ReactNode;
 }
+
+function Button({ onClick, type, children }: Props) {
+  return (
+    <button onClick={onClick} type={type} className={style.button}>
+      {children}
+    </button>
+  );
+}
+
+// class Button1 extends React.Component<{
+//   children: string;
+//   type?: "button" | "submit" | "reset" | undefined;
+//   onClick?: () => void;
+// }> {
+//   render() {
+//     const { type = "button", onClick } = this.props;
+//     return (
+//       <button onClick={onClick} type={type} className={style.button}>
+//         {this.props.children}
+//       </button>
+//     );
+//   }
+// }
 
 export default Button;
--- alura-studies/src/components/Form/index.tsx
+++ alura-studies/src/components/Form/index.tsx
@@ -1,66 +1,116 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Form.module.scss";
 import Button from "../Button";
 import { ITask } from "../../types/task";
 import { v4 as uuidv4 } from "uuid";
 
-class Form extends React.Component<{
+interface Props {
   setTasks: React.Dispatch<React.SetStateAction<ITask[]>>;
-}> {
-  state = {
-    task: "",
-    time: "00:00",
-  };
-
-  addTask(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    this.props.setTasks((oldTasks) => [
-      ...oldTasks,
-      { ...this.state, selected: false, completed: false, id: uuidv4() },
-    ]);
-    this.setState({
-      task: "",
-      time: "00:00",
-    });
-  }
-
-  render() {
-    return (
-      <form className={style.newTask} onSubmit={this.addTask.bind(this)}>
-        <div className={style.inputContainer}>
-          <label htmlFor="task">Add a new study</label>
-          <input
-            type="text"
-            name="task"
-            id="task"
-            value={this.state.task}
-            onChange={(event) =>
-              this.setState({ ...this.state, task: event.target.value })
-            }
-            placeholder="What do you want to study?"
-            required
-          />
-        </div>
-        <div className={style.inputContainer}>
-          <label htmlFor="time"></label>
-          <input
-            type="time"
-            step="1"
-            name="time"
-            value={this.state.time}
-            onChange={(event) =>
-              this.setState({ ...this.state, time: event.target.value })
-            }
-            id="time"
-            min="00:00:00"
-            max="01:30:00"
-            required
-          />
-        </div>
-        <Button type="submit">Add</Button>
-      </form>
-    );
-  }
 }
+
+function Form({ setTasks }: Props) {
+  const [task, setTask] = useState("");
+  const [time, setTime] = useState("00:00");
+
+  function addTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    setTasks((oldTasks) => [
+      ...oldTasks,
+      { task, time, selected: false, completed: false, id: uuidv4() },
+    ]);
+    setTask("");
+    setTime("00:00");
+  }
+  return (
+    <form className={style.newTask} onSubmit={addTask}>
+      <div className={style.inputContainer}>
+        <label htmlFor="task">Add a new study</label>
+        <input
+          type="text"
+          name="task"
+          id="task"
+          value={task}
+          onChange={(event) => setTask(event.target.value)}
+          placeholder="What do you want to study?"
+          required
+        />
+      </div>
+      <div className={style.inputContainer}>
+        <label htmlFor="time"></label>
+        <input
+          type="time"
+          step="1"
+          name="time"
+          value={time}
+          onChange={(event) => setTime(event.target.value)}
+          id="time"
+          min="00:00:00"
+          max="01:30:00"
+          required
+        />
+      </div>
+      <Button type="submit">Add</Button>
+    </form>
+  );
+}
+
+// class Form1 extends React.Component<{
+//   setTasks: React.Dispatch<React.SetStateAction<ITask[]>>;
+// }> {
+//   state = {
+//     task: "",
+//     time: "00:00",
+//   };
+
+//   addTask(event: React.FormEvent<HTMLFormElement>) {
+//     event.preventDefault();
+//     this.props.setTasks((oldTasks) => [
+//       ...oldTasks,
+//       { ...this.state, selected: false, completed: false, id: uuidv4() },
+//     ]);
+//     this.setState({
+//       task: "",
+//       time: "00:00",
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <form className={style.newTask} onSubmit={this.addTask.bind(this)}>
+//         <div className={style.inputContainer}>
+//           <label htmlFor="task">Add a new study</label>
+//           <input
+//             type="text"
+//             name="task"
+//             id="task"
+//             value={this.state.task}
+//             onChange={(event) =>
+//               this.setState({ ...this.state, task: event.target.value })
+//             }
+//             placeholder="What do you want to study?"
+//             required
+//           />
+//         </div>
+//         <div className={style.inputContainer}>
+//           <label htmlFor="time"></label>
+//           <input
+//             type="time"
+//             step="1"
+//             name="time"
+//             value={this.state.time}
+//             onChange={(event) =>
+//               this.setState({ ...this.state, time: event.target.value })
+//             }
+//             id="time"
+//             min="00:00:00"
+//             max="01:30:00"
+//             required
+//           />
+//         </div>
+//         <Button type="submit">Add</Button>
+//       </form>
+//     );
+//   }
+// }
 
 export default Form;
